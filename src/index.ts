0639e04a5bf84b43214032ec6ef632d2fd5d6c79
vertocode <evertonvanoni1@gmail.com>
2023-08-14 19:42:33 -0300

    fix: solve cors and bodyParse

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import express, { Express, Request, Response } from 'express'
-import * as fs from "fs"
-import path from "path";
+import * as fs from 'fs'
+import path from 'path'
+import cors from 'cors'
+import bodyParser from 'body-parser'
 
 const app: Express = express()
+const port: number = 3000
+
+app.use(cors)
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/', (req: Request, res: Response): void => {
     res.send('Rune Fast API')
@@ -18,6 +25,10 @@ app.get('/categories', (req: Request, res: Response): void => {
         console.error('Error reading categories.json:', error)
         res.status(500).send('Internal Server Error')
     }
+})
+
+app.listen(port, (): void => {
+    console.log(`⚡️[server]: Rune Fast API is running at http://localhost:${port}`)
 })
 
 module.exports = app
