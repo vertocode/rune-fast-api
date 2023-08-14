639874bb24001b0e24d28c8c9899528c01f8ee90
vertocode <evertonvanoni1@gmail.com>
2023-08-14 19:24:36 -0300

    feat: init app

---
+++ src/index.js
@@ -0,0 +1,50 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const fs = __importStar(require("fs"));
+const path_1 = __importDefault(require("path"));
+const app = (0, express_1.default)();
+const port = 3000;
+app.get('/', (req, res) => {
+    res.send('Rune Fast API');
+});
+app.get('/categories', (req, res) => {
+    const filePath = path_1.default.join(__dirname, 'data', 'categories.json');
+    try {
+        const categories = fs.readFileSync(filePath, 'utf-8');
+        res.send(categories);
+    }
+    catch (error) {
+        console.error('Error reading categories.json:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+app.listen(port, () => {
+    console.log(`⚡️[server]: Rune Fast API is running at http://localhost:${port}`);
+});
